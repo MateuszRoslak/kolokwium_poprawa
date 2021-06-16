1d02031453a2b0b9d2834c0f6e1879741cfa1848
MateuszRoslak <222554@edu.p.lodz.pl>
2021-06-16 17:56:59 +0200

    Designed tests cases

--- testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/reservation/ReservationTest.java
@@ -1,4 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+package edu.iis.mto.testreactor.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ReservationTest {
-  
+
+    @Test
+    public void itCompiles(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void happyPath(){}
+
+    @Test
+    public void itShouldReturnsEmptyOfferWhenNoItemsGiven(){}
+
+    @Test
+    public void itShouldThrowErrorWhenTryingToAddProductToClosedReservation(){}
+
+    @Test
+    public void itShouldThrowErrorWhenAddedProductIsNotAvailable(){}
+
+    @Test
+    public void itShouldThrowErrorWhenTryingToCloseAlreadyClosedReservation(){}
+
+    @Test
+    public void itShouldSetAllReservationDataCorrectly(){};
+
+    @Test
+    public void itShouldReturnReservationContainingGivenItem(){};
 }
